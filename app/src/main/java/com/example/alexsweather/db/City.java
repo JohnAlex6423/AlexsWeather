977b5f40ad251717c50af4846c53bdd97591d2f6
Alex <Alex@qq.com>
2017-12-08 22:24:38 +0800

    创建数据库配置

---
+++ app/src/main/java/com/example/alexsweather/db/City.java
@@ -0,0 +1,47 @@
+package com.example.alexsweather.db;
+
+/**
+ * Created by LaoZhao on 2017/12/8.
+ */
+
+public class City {
+    private int id;
+
+    private String cityName;
+
+    private int cityCode;
+
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
